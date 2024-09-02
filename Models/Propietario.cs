4de52c120e8b2856ec8ee8d71f778e344a9edcfe
Camilobarre <barrecami@gmail.com>
2024-09-02 07:14:58 -0500

    feat: Properties created

--- Models/Propietario.cs
+++ Models/Propietario.cs
@@ -7,6 +7,12 @@ namespace Taller.Models
 {
     public class Propietario
     {
-        
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Apellido { get; set; }
+        public required string NumeroIdentificcaion { get; set; }
+        public required string Direccion { get; set; }
+        public required string Telefono { get; set; }
+        public required string Correo { get; set; }
     }
 }
