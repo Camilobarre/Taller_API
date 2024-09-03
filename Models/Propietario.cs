fd461e3a7339028a426b29c5275fec5b834c66e4
Camilobarre <barrecami@gmail.com>
2024-09-03 07:01:28 -0500

    feat: Data annotations added

--- Models/Propietario.cs
+++ Models/Propietario.cs
@@ -10,11 +10,28 @@ namespace Taller.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [StringLength(100)]
+        [MinLength(5, ErrorMessage = "The Name field must be more than {5} characters.")]
+        [MaxLength(125, ErrorMessage = "The Name field must be less than {125} characters.")]
         public required string Nombre { get; set; }
         public required string Apellido { get; set; }
-        public required string NumeroIdentificcaion { get; set; }
+        public required string NumeroIdentificaion { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The Name field must be less than {125} characters.")]
+        [Url(ErrorMessage = "The FotoPerfil field must be a valid URL.")]
+        public required string FotoPerfil { get; set; }
         public required string Direccion { get; set; }
+
+
+        [MinLength(5, ErrorMessage = "The Phone field must be more than {5} characters.")]
+        [MaxLength(25, ErrorMessage = "The Phone field must be less than {125} characters.")]
+        [Phone(ErrorMessage = "Invalid Phone Format.")]
         public required string Telefono { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Format.")]
+        [MinLength(5, ErrorMessage = "The Email field must be more than {5} characters.")]
+        [MaxLength(255, ErrorMessage = "The Email field must be less than {125} characters.")]
         public required string Correo { get; set; }
     }
 }
