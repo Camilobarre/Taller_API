7fc7387b89573b68bdf82a057be58ab78fce4076
Camilobarre <barrecami@gmail.com>
2024-09-04 07:08:55 -0500

    feat: Endpoint created

--- Program.cs
+++ Program.cs
@@ -1,41 +1,34 @@
 using DotNetEnv;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Taller.Data;
-
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
+// Conexión con la base de datos
 Env.Load();
-
 var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
 var dbDatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
 var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
 var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
-
 var conectionDB = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
-
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseMySql(conectionDB, ServerVersion.Parse("8.0.20-mysql")));
 
-var app = builder.Build();
-// Add services to the container.
-
+// Servicios.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Canal de peticiones HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
-app.UseWelcomePage(); //Página por defecto de bienvenida
+// app.UseWelcomePage(); // Página de bienvenida
 
 app.UseAuthorization();
 
